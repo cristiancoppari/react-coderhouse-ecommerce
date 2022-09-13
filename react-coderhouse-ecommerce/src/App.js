f45b4b5939cb13b65bc125d39f464ea310725862
Cristian <cristian.coppari@gmail.com>
2022-09-12 22:52:05 -0300

    borro el componente cart porque viene mas adelante

--- react-coderhouse-ecommerce/src/App.js
+++ react-coderhouse-ecommerce/src/App.js
@@ -3,7 +3,7 @@ import NavBar from "./components/NavBar/NavBar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
-import Cart from "./components/Cart/Cart";
+// import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<ItemListContainer />} />
         <Route path="/category/:categoryId" element={<ItemListContainer />} />
         <Route path="/item/:id" element={<ItemDetailContainer />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<div>Carrito</div>} />
 
         {/* TODO: Agregar pagina de error */}
         {/* Si entro a /category/sarasa no funciona */}
